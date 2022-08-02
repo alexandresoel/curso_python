7687f74873c8892a9cc5e936d7d389ef2405cec5
alexandresoel <alexandre_soel@hotmail.com>
2022-08-02 20:29:17 -0300

    Atualizações de aula até o momento 02/08/2022

--- aula11/aula11.py
+++ aula11/aula11.py
@@ -1,8 +1,21 @@
 """
 Operadores Relacionais
-== >= > < <= !=
+== igualdade
+>= maior ou igual
+>  maior que
+<  menor que
+<= menor ou igual
+!= diferente
 """
-num_1 = 2
-num_2 = 2
-expressao = ( num_1 == num_2)
-print(expressao)
+nome = input("Qual o seu nome? ")
+idade = int(input("Qual é a sua idade? "))
+valor = float(input("Qual o valor do emprestimo? "))
+
+# Limite para pegar emprestimo
+idade_limite = 18
+
+if(idade>=idade_limite):
+    print(f'{nome} pode pegar o empréstimo.')
+    print(f'Você pegou R$ {valor:.2f} e terá que pagar R${valor * 1.25}')
+else:
+    print(f'{nome} NÃO PODE PEGAR O EMPRESTIMO.')
